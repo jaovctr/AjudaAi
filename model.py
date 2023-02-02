b71b823d2fb6c382811a99667584c9146a63d669
Caio Feitosa <franciscocaiof@gmail.com>
2023-02-01 22:02:14 -0300

    Ajustes na página de lista de demandas para conexao com o back-end.

---
+++ model.py
@@ -0,0 +1,23 @@
+demandas = [
+    {
+        'titulo': 'Monitoria sobre bioquímica',
+        'tags': ['BIOLOGIA - Bioquímica', 'BIOLOGIA - Biologia I'],
+        'tipo': 'Aberto a membros',
+        'descricao': 'Gostaria de alguém para me auxiliar sobre estrutura e função metabólica de componentes celulares.',
+        'status': 'Em aberto',
+    },
+    {
+        'titulo': 'Revisão do Abstract',
+        'tags': ['INGLÊS: A1', 'INGLÊS: Gramática', 'INGLÊS: Revisão abstract'],
+        'tipo': 'Individual',
+        'descricao': 'Possuo conhecimento básico de inglês e procuro monitoria para revisar comigo o abstract do meu artigo.',
+        'status': 'Em aberto'
+    },
+    {
+        'titulo': 'Monitoria de programação',
+        'tags': ['COMPUTAÇÃO: Algoritmo', 'COMPUTAÇÃO: Programação Estruturada', 'COMPUTAÇÃO: Linguagem C'],
+        'tipo': 'Aberto a membros',
+        'descricao': 'Sou estudante de bioinformática e preciso aprender scripts em python, mas possuo dificuldade em lógica de programação.',
+        'status': 'Finalizado'
+    }
+]
