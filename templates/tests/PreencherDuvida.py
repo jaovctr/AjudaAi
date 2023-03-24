fde934af9603d497f675eac4bb5d4db928032834
João Victor <33011283+jaovctr@users.noreply.github.com>
2023-03-24 12:23:25 -0300

    Update PreencherDuvida.py

--- templates/tests/PreencherDuvida.py
+++ templates/tests/PreencherDuvida.py
@@ -15,7 +15,7 @@ options.add_argument("--disable-extensions")
 driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/bin/chromedriver')
 class TestPreencherinformaes():
   def setup_method(self, method):
-    self.driver = webdriver.Chrome()
+    #self.driver = webdriver.Chrome()
     self.vars = {}
   
   def teardown_method(self, method):
