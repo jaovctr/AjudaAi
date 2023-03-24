400f24c29510bcd9717f66b5f069c4158ac2ebbf
João Victor <33011283+jaovctr@users.noreply.github.com>
2023-03-24 12:33:01 -0300

    Update test_cadastrodemanda.py

--- templates/tests/test_cadastrodemanda.py
+++ templates/tests/test_cadastrodemanda.py
@@ -4,22 +4,23 @@ import time
 import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> bd6f21f6d65254e4efbe5b8dbeab1dc9d8540f68
-=======
->>>>>>> bd6f21f6d65254e4efbe5b8dbeab1dc9d8540f68
+#from selenium.webdriver.common.action_chains import ActionChains
+#from selenium.webdriver.support import expected_conditions
+#from selenium.webdriver.support.wait import WebDriverWait
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+#from selenium.webdriver.chrome.options import Options
+options = Options()
+options.add_argument("--headless")
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('disable-infobars')
+options.add_argument("--disable-extensions")
+driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/bin/chromedriver')
 class TestCadastrodemanda():
   def setup_method(self, method):
-    self.driver = webdriver.Chrome()
+    self.driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/bin/chromedriver')
     self.vars = {}
   
   def teardown_method(self, method):
