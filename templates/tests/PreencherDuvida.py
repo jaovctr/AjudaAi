822f49d41be5650273edd558df495c17140c215c
João Victor <33011283+jaovctr@users.noreply.github.com>
2023-03-24 12:05:29 -0300

    Update PreencherDuvida.py

--- templates/tests/PreencherDuvida.py
+++ templates/tests/PreencherDuvida.py
@@ -3,13 +3,14 @@ import pytest
 import time
 import json
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
+from selenium.webdriver.chrome.options import Options
+options = Options()
+options.add_argument("--headless")
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('disable-infobars')
+options.add_argument("--disable-extensions")
 class TestPreencherinformaes():
   def setup_method(self, method):
     self.driver = webdriver.Chrome()
