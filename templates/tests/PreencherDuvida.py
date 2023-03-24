bd10e0ff765139637a58dff0ef9c406caf9eaecc
João Victor <33011283+jaovctr@users.noreply.github.com>
2023-03-24 12:19:49 -0300

    Update PreencherDuvida.py

--- templates/tests/PreencherDuvida.py
+++ templates/tests/PreencherDuvida.py
@@ -12,6 +12,7 @@ options.add_argument('--disable-gpu')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('disable-infobars')
 options.add_argument("--disable-extensions")
+driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/bin/chromedriver')
 class TestPreencherinformaes():
   def setup_method(self, method):
     self.driver = webdriver.Chrome()
